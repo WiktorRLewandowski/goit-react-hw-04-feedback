5453e8def710cee334fc757bc6e6f6a96184f720
Wiktor Lewandowski <lewandowski.wiktor@yahoo.com>
2023-07-09 15:50:52 +0200

    hook refactor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,94 +1,63 @@
-import { Component } from "react";
+import { useState } from 'react';
 import { Statistics } from './Statistics'
 import { FeedbackOptions } from './FeedbackOptions'
 import { Section, Notification } from "./Section";
 import PropTypes from 'prop-types';
 
-class App extends Component {
+export default function App()  {
 
-  static defaultProps = {
-    step: 1,
-    initialValue: 0,
-    total: 0,
-    percentage: 0
-  }
+  const [value, setValue] = useState({good: 0, neutral: 0, bad: 0});
+  let total = Object.values(value);
 
-  // constructor() {
-  //   super();
-  //   this.updateFeedback = this.updateFeedback.bind(this);
-  // }
- 
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  }
-
-  static propTypes = {
-    step: PropTypes.number,
-    initialValue: PropTypes.number,
-    total: PropTypes.number,
-    percentage: PropTypes.number,
-    good: PropTypes.number,
-    neutral: PropTypes.number,
-    bad: PropTypes.number,
-  }
-
-  updateFeedback = (ev) => {
-    this.setState((prevState) => ({ 
-      [ev]: prevState[ev] + this.props.step
+  const updateFeedback = (ev) => {
+    setValue((prevState) => ({ 
+      ...prevState,
+      [ev]: prevState[ev] + 1
     }))
   }
+
+  total = total.reduce((acc, curr) => acc + curr)
   
-  totalFeedback = e => (
-    this.total = this.state.good + this.state.bad + this.state.neutral
+  const positiveFeedback = () => total > 0 ? Math.round((value.good / total) * 100) : 0
+  
+  const {good, neutral, bad} = value
+  const percentage = positiveFeedback();
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      <Section title='Please, leave feedback!'>
+      <FeedbackOptions
+      options={Object.keys(value)}
+      onLeaveFeedback={updateFeedback}/>
+      {total 
+      ? <Statistics
+      good={good}
+      neutral={neutral}
+      bad={bad}
+      total={total}
+      percentage={percentage}/>
+      : <Notification message="There is no feedback :("/>
+      }      
+      </Section>
+    </div>
   );
-
-  positiveFeedback = () => 
-  this.total > 0 
-  ? Math.round((this.state.good / this.total) * 100) 
-  : 0;
-
-  // totalFeedback = e => console.log(this.state.reduce((acc, curr)=> acc+curr, this.props.initialValue))
-
-  render() {
-    const {good, neutral, bad} = this.state
-    const total = this.totalFeedback();
-    const percentage = this.positiveFeedback();
-
-    return (
-      <div
-        style={{
-          // height: '100vh',
-
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101'
-        }}
-      >
-        <Section title='Please, leave feedback!'>
-
-        <FeedbackOptions
-        options={Object.keys(this.state)}
-        onLeaveFeedback={this.updateFeedback}/>
-
-        {this.totalFeedback() 
-        ? <Statistics
-        good={good}
-        neutral={neutral}
-        bad={bad}
-        total={total}
-        percentage={percentage}/>
-        : <Notification message="There is no feedback :("/>
-        }
-        
-        </Section>
-      </div>
-    );
-  }
 }
 
-export default App
+App.propTypes = {
+  step: PropTypes.number,
+  initialValue: PropTypes.number,
+  total: PropTypes.number,
+  percentage: PropTypes.number,
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+}
